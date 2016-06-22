ea17e89df67a83c0185be5fcbab3dec53ece8212
BugsBunyl5 <tomjmiller@outlook.com>
2016-06-21 20:28:15 -0800

    What's up, everybody! still haven't made an executable (app) for this game, but I am about to! Stay tuned!

--- a bit racey.py
+++ a bit racey.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -8,9 +9,17 @@ display_height = 600
 
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
+red = (200, 0, 0)
+green = (0, 200, 0)
+bright_red = (255, 0, 0)
+bright_green =(0, 255, 0)
 
-car_width = 70
+colorlist = [black, red, green, bright_green, bright_red]
+
+# block_color = (53, 115, 255)
+block_color = random.choice(colorlist)
+
+car_width = 73
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A Bit Racey')
@@ -19,12 +28,24 @@ clock = pygame.time.Clock()
 carImg = pygame.image.load('C:\\Users\\tomjm\\PycharmProjects\\PyGame\\racecar.png')
 
 
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: " + str(count), True, red)
+    gameDisplay.blit(text, (0, 0))
+
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
+
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
+
 
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf', 115)
@@ -42,11 +63,60 @@ def message_display(text):
 def crash():
     message_display('You Crashed')
 
+
+def button(msg, x, y, w, h, ic, ac, action = None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            if action == "play":
+                game_loop()
+            elif action == "quit":
+                pygame.quit()
+                quit()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    gameDisplay.blit(textSurf, textRect)
+
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("GO!", 150, 450, 100, 50, green, bright_green, "play")
+        button("Quit", 550, 450, 100, 50, red, bright_red, "quit")
+
+        pygame.display.update()
+        clock.tick(15)
+
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
 
     x_change = 0
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 4
+    thing_width = 100
+    thing_height = 100
+
+    dodged = 0
 
     gameExit = False
 
@@ -67,14 +137,34 @@ def game_loop():
         x += x_change
 
         gameDisplay.fill(white)
+
+        things(thing_startx, thing_starty, thing_width, thing_height, block_color)
+        thing_starty += thing_speed
         car(x, y)
+        things_dodged(dodged)
 
         if x > display_width - car_width or x < 0:
             crash()
 
-        pygame.display.update()
-        clock.tick(60)
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
+            dodged += 1
+            thing_speed += 0.1
+            thing_width += (dodged * 1.2)
 
+        if y < thing_starty + thing_height:
+            print('dodge')
+
+            if thing_startx < x < thing_startx + thing_width or thing_startx < x + car_width < thing_startx + thing_width:
+                print('crash')
+                crash()
+
+        pygame.display.update()
+        clock.tick(200)
+
+
+game_intro()
 game_loop()
 pygame.quit()
 quit()
